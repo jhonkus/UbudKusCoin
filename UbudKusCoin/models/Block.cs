cc75c5acd1aaea05cde895f7f2beb3affb9fd6ec
jhonkus <putukn@gmail.com>
2020-11-05 21:30:01 +0800

    modify constructor and add overloading

--- UbudKusCoin/models/Block.cs
+++ UbudKusCoin/models/Block.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
-using Utils;
+using System.Text;
 
 namespace Models
 {
@@ -9,40 +9,59 @@ namespace Models
     {
         public int Height { get; set; }
         public Int64 TimeStamp { get; set; }
-        public byte[] PrevHash { get; set; }
-        public byte[] Hash { get; set; }
-        public Transaction[] Transactions { get; set; }
-        public string Creator { get; set; }
+        public string PrevHash { get; set; }
+        public string Hash { get; set; }
+        public string Transactions { get; set; }
 
-        public Block(int height, byte[] prevHash, Transaction[] transactions, string creator)
+
+        public Block()
+        {
+        }
+
+        /**
+        To enerate genesis blok
+        **/
+        public static Block Genesis()
+        {
+            var ts = new DateTime(2019, 10, 24);
+            var genesisTrx = "Genesis Block created by P.Kusuma  on 2019 10 24";
+            var hash = Block.GetHash(ts.Ticks, "-", genesisTrx);
+            //var block = new Block(1, ts.Ticks, Convert.ToBase64String(Encoding.ASCII.GetBytes("-")), hash, Convert.ToBase64String(Encoding.ASCII.GetBytes(genesisTrx)));
+            var block = new Block(1, ts.Ticks, Convert.ToBase64String(Encoding.ASCII.GetBytes("-")), hash, genesisTrx);
+            return block;
+        }
+
+
+        // constructure
+        public Block(int height, Int64 timestamp, string lastHash, string hash, string transactions)
         {
             this.Height = height;
-            this.PrevHash = prevHash;
-            this.TimeStamp = DateTime.Now.Ticks;
+            this.TimeStamp = timestamp;
+            this.PrevHash = lastHash;
+            this.Hash = hash;
             this.Transactions = transactions;
-            this.Hash = GenerateHash();
-            this.Creator = creator;
         }
 
-        public byte[] GenerateHash()
+
+        public static string GetHash(Int64 timestamp, string lastHash, string transactions)
         {
-            var sha = SHA256.Create();
-            byte[] timeStamp = BitConverter.GetBytes(TimeStamp);
-
-            var transactionHash = Transactions.ConvertToByte();
-
-            byte[] headerBytes = new byte[timeStamp.Length + PrevHash.Length + transactionHash.Length];
-
-            Buffer.BlockCopy(timeStamp, 0, headerBytes, 0, timeStamp.Length);
-            Buffer.BlockCopy(PrevHash, 0, headerBytes, timeStamp.Length, PrevHash.Length);
-            Buffer.BlockCopy(transactionHash, 0, headerBytes, timeStamp.Length + PrevHash.Length, transactionHash.Length);
-
-            byte[] hash = sha.ComputeHash(headerBytes);
-
-            return hash;
-
+            SHA256 sha256 = SHA256.Create();
+            var strSum = timestamp + lastHash + transactions;
+            byte[] sumBytes = Encoding.ASCII.GetBytes(strSum);
+            byte[] hashBytes = sha256.ComputeHash(sumBytes);
+            return Convert.ToBase64String(hashBytes);
         }
 
+        public  Block(Block lastBlock, string transactions)
+        {
+            var lastHeight = lastBlock.Height;
+            var lastHash = lastBlock.Hash;
+            this.Height = lastHeight + 1;
+            this.TimeStamp = DateTime.Now.Ticks;
+            this.PrevHash = lastHash;
+            this.Transactions = transactions;
+            this.Hash = Block.GetHash(this.TimeStamp, lastHash, transactions);
+        }
 
     }
 }
