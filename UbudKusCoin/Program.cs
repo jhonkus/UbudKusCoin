0b383ccc96be3c73c8f8ce40c9fe3b9e0a4560a3
jhonkus <putukn@gmail.com>
2020-11-05 15:00:35 +0800

    Add menu

--- UbudKusCoin/Program.cs
+++ UbudKusCoin/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Models;
 using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace Main
@@ -11,76 +12,102 @@ namespace Main
 
             // Make blockchain
             var bc = new Blockchain();
-           
-            //Create new transaction
-            var trx1 = new Transaction
+
+
+            int selection = 0;
+            while (selection != 100)
             {
-                Sender = "Genesis Account",
-                Recipient = "Ricardo",
-                Amount = 300,
-                Fee = 0.001
-            };
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("Geting Genesis Block");
+                        var genesisBlock = bc.GetGenesisBlock();
+                        Console.WriteLine(JsonConvert.SerializeObject(genesisBlock, Formatting.Indented));
 
-            //Create new transaction
-            var trx2 = new Transaction
-            {
-                Sender = "Genesis Account",
-                Recipient = "Frodo",
-                Amount = 250,
-                Fee = 0.001
-            };
+                        break;
+                    case 2:
+                        var lastBlock = bc.GetLastBlock();
+                        Console.WriteLine("Geting Last Block");
+                        Console.WriteLine(JsonConvert.SerializeObject(lastBlock, Formatting.Indented));
+                        break;
 
-            //Create new transaction
-            var trx3 = new Transaction
-            {
-                Sender = "Ricardo",
-                Recipient = "Madona",
-                Amount = 20,
-                Fee = 0.0001
-            };
+                    case 3:
+                        Console.WriteLine("Sender name (type any name): ");
+                        string from = Console.ReadLine();
 
-            //create list of transactions
-            var lsTrx = new List<Transaction>
-            {
-                trx1,
-                trx2
-            };
+                        Console.WriteLine("Please enter the Recipient name (type any name):");
+                        string to = Console.ReadLine();
 
-            var transactions = lsTrx.ToArray();
-            bc.AddBlock(transactions);
+                        Console.WriteLine("Please enter the amount");
+                        string strAmount = Console.ReadLine();
+                        int amount = 0;
+                        try
+                        {
+                            amount = int.Parse(strAmount);
+                            var trx = new Transaction
+                            {
+                                Sender = from,
+                                Recipient = to,
+                                Amount = 20,
+                                Fee = 0.1
+                            };
 
-            lsTrx = new List<Transaction>
-            {
-                trx3         
-            };
+                            bc.TrxPool.Add(trx);
+                            Console.WriteLine(" Transaction added to transaction pool.");
 
-            transactions = lsTrx.ToArray();
-            bc.AddBlock(transactions);
-
-            //Print all blocks
-            bc.PrintBlocks();
-
-            //check balance for each account account
-            var balance = bc.GetBalance("Genesis Account");
-            Console.WriteLine("Genesis Account balance: {0}", balance);
-
-            balance = bc.GetBalance("Ricardo");
-            Console.WriteLine("Ricardo balance: {0}", balance);
-
-            balance = bc.GetBalance("Frodo");
-            Console.WriteLine("Frodo  balance: {0}", balance);
+                        }
+                        catch
+                        {
+                            Console.WriteLine(" please check amount.");
+                        }
 
 
-            balance = bc.GetBalance("Madona");
-            Console.WriteLine("Madona balance: {0}", balance);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Crate Block");
+                        if (bc.TrxPool.Count <= 0)
+                        {
+                            Console.WriteLine("No transaction in pool, please create transaction first!");
+                        }
+                        else
+                        {
+                            var transactions = bc.TrxPool.ToArray();
+                            bc.AddBlock(transactions);
+                            bc.TrxPool = new List<Transaction>();
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\n\nBlock List\n- - - - - - -");
+                        bc.PrintBlocks();
+                        break;
+
+                    case 9:
+                        Console.WriteLine("Exit");
+                        Environment.Exit(-1);
+                        break;
+                }
+
+                Console.WriteLine("\n\nUBUD PKUS Crypto v0.0.1\n=========================");
+                Console.WriteLine("1. Get Genesis Block");
+                Console.WriteLine("2. Get Last Block");
+                Console.WriteLine("3. Add a transaction");
+                Console.WriteLine("4. Create Block");
+                Console.WriteLine("5. Print Blocks");
+                Console.WriteLine("9. Exit");
+                Console.WriteLine("=========================");
+
+                Console.WriteLine("Please selectd menu by input the number!");
+                string action = Console.ReadLine();
+                try{
+                    selection = int.Parse(action);
+                }catch{
+                    Console.WriteLine("--- Please input correct number! ---");
+                }
+            }
 
             Console.ReadKey();
         }
-
-
-
     }
-
-
-
 }
