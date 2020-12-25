22d9824895d8b5e4608101ca0e0a144745e8cdab
jhonkus <putukn@gmail.com>
2020-12-25 22:14:17 +0800

    Add timestamp to Transaction

--- UbudKusCoin/models/Transaction.cs
+++ UbudKusCoin/models/Transaction.cs
@@ -8,6 +8,7 @@ namespace Models
     [Serializable]
     public class Transaction
     {
+        public long TimeStamp { get; set; }
         public string Sender { set; get; }
         public string Recipient { set; get; }
         public double Amount { set; get; }
