3b279f8efb8cacf10eecd30b236103fd4c683f33
jhonkus <putukn@gmail.com>
2020-12-27 13:59:18 +0800

    Modify Program.cs, add print genesis block, last block and transaction history

--- UbudKusCoin/Program.cs
+++ UbudKusCoin/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Models;
 using System;
-using System.Collections.Generic;
 
 namespace Main
 {
@@ -9,78 +8,98 @@ namespace Main
         static void Main()
         {
 
-            // Make blockchain
+            // Initialize blockchain
             var bc = new Blockchain();
-           
-            //Create new transaction
+
+
+            // Create new transaction
             var trx1 = new Transaction
             {
+                TimeStamp = DateTime.Now.Ticks,
                 Sender = "Genesis Account",
                 Recipient = "Ricardo",
                 Amount = 300,
                 Fee = 0.001
             };
 
+            // add transaction to pool
+            bc.AddTransactionToPool(trx1);
+
+
             //Create new transaction
             var trx2 = new Transaction
             {
+                TimeStamp = DateTime.Now.Ticks,
                 Sender = "Genesis Account",
                 Recipient = "Frodo",
                 Amount = 250,
                 Fee = 0.001
             };
 
-            //Create new transaction
+            // add transaction to pool
+            bc.AddTransactionToPool(trx2);
+
+            // add a block to blockchain
+            bc.CreateBlock();
+
+            // clear transaction pool after block created
+            bc.ClearPool();
+
+
+            // create new transaction again
             var trx3 = new Transaction
             {
+                TimeStamp = DateTime.Now.Ticks,
                 Sender = "Ricardo",
                 Recipient = "Madona",
                 Amount = 20,
                 Fee = 0.0001
             };
 
-            //create list of transactions
-            var lsTrx = new List<Transaction>
-            {
-                trx1,
-                trx2
-            };
+            // add transaction to pool
+            bc.AddTransactionToPool(trx3);
 
-            var transactions = lsTrx.ToArray();
-            bc.AddBlock(transactions);
 
-            lsTrx = new List<Transaction>
-            {
-                trx3         
-            };
+            // forging a block
+            bc.CreateBlock();
 
-            transactions = lsTrx.ToArray();
-            bc.AddBlock(transactions);
+            // clear transaction pool again
+            bc.ClearPool();
 
-            //Print all blocks
+            // -----------------------------------------
+            // till here we create 3 transactions and 2 blocks.
+            // all bloks already added to blockchain
+            // ----------------------------------------
+
+
+            // Now lets get some data from blockchain
+
+            // print genesis block
+            bc.PrintGenesisBlock();
+
+            // print last block
+            bc.PrintLastBlock();
+
+            //check balance for Ricardo
+            bc.PrintBalance("Ricardo");
+
+
+            //check balance for Frodo
+            bc.PrintBalance("Frodo");
+
+
+            //check balance for Madona
+            bc.PrintBalance("Madona");
+
+
+
+            bc.PrintTransactionHistory("Ricardo");
+
+            // print all block in Blockchain
             bc.PrintBlocks();
 
-            //check balance for each account account
-            var balance = bc.GetBalance("Genesis Account");
-            Console.WriteLine("Genesis Account balance: {0}", balance);
-
-            balance = bc.GetBalance("Ricardo");
-            Console.WriteLine("Ricardo balance: {0}", balance);
-
-            balance = bc.GetBalance("Frodo");
-            Console.WriteLine("Frodo  balance: {0}", balance);
-
-
-            balance = bc.GetBalance("Madona");
-            Console.WriteLine("Madona balance: {0}", balance);
-
+            // press enter to close    
             Console.ReadKey();
         }
-
-
-
     }
-
-
-
 }
