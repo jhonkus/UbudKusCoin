44407b8cc3f2bd6793808e5d9713a18ce1de3315
jhonkus <putukn@gmail.com>
2021-01-12 01:40:22 +0800

    update DesktopWallet

--- DesktopWallet/Program.cs
+++ DesktopWallet/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using grpcservice.Protos;
+
 
 namespace DesktopWallet
 {
@@ -6,7 +14,29 @@ namespace DesktopWallet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serverAddress = "https://localhost:5002";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+
+                // The following statement allows you to call insecure services. To be used only in development environments.
+                AppContext.SetSwitch(
+                    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                serverAddress = "http://localhost:5002";
+            }
+            //var channel = GrpcChannel.ForAddress(serverAddress);
+
+
+            var channel = GrpcChannel.ForAddress(serverAddress, new GrpcChannelOptions
+            {
+                HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+            });
+
+
+            // show menu
+            Menu.DisplayMenu(channel);
         }
+
     }
+
+
 }
